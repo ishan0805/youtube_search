7f4a90d708ac6582574182dbd4baa1f0a0dd3c7f
Ishan Kumar Srivastava <57336738+ishan0805@users.noreply.github.com>
2023-01-30 00:16:09 +0530

    added docker file

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-from multiprocessing.connection import wait
-from timeit import repeat
 from fastapi import FastAPI
 from database import  engine,Base
 import uvicorn
@@ -28,7 +26,7 @@ app.add_middleware(
 )
 
 @app.on_event("startup")
-@repeat_every(seconds=5,wait_first=True)
+@repeat_every(seconds=20,wait_first=True)
 async def start():
     await get_youtube_data()
 
